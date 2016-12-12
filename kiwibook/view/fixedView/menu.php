2d44ec0844e52dd1bc79d6c12cfb0278743f2859
pierreAroban <pierre.aroban@gmail.com>
2016-12-12 14:13:56 +0100

    menu

--- kiwibook/view/fixedView/menu.php
+++ kiwibook/view/fixedView/menu.php
@@ -1,16 +1,18 @@
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="kiwibook.php?action=index">Index</a>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="kiwibook.php?action=index">Index</a>
+        </div>
+        <?php if ($context::getSessionAttribute('id')) { ?>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="kiwibook.php?action=profil">Profil</a></li>
+                <li><a href="kiwibook.php?action=showUsers">Liste d'amis</a>
+                <li><a href="kiwibook.php?action=showMessage">Mur</a></li>
+                <li><a href="#" id="chat-button-window">Chat</a></li>
+            </ul>
+        <?php } ?>
+        <ul class="nav navbar-nav navbar-right">
+            <li> <?php include('loginForm.php'); ?> </li>
+        </ul>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="kiwibook.php?action=profil">Profil</a></li>
-          <li><a href="kiwibook.php?action=showUsers">Liste d'amis</a>
-          <li><a href="kiwibook.php?action=showMessage">Mur</a></li>
-          <li><a href="#" id="chat-button-window">Chat</a></li>
-      </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li> <?php include('loginForm.php'); ?> </li>
-    </ul>
-  </div>
 </nav>
