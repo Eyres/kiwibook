d6c9423d1ac171f0f9b9e50336210a50a148c1ad
pierreAroban <pierre.aroban@gmail.com>
2016-12-21 21:03:49 +0100

    Menu bootstrap fonctionnel

--- kiwibook/view/fixedView/loginForm.php
+++ kiwibook/view/fixedView/loginForm.php
@@ -1,21 +1,17 @@
 <form method="post" action="kiwibook.php?action=login">
     <div class="form-group">
-        <div class="row">
-            <label class="col-sm-3" >Pseudo:</label>
-            <div class = "col-sm-9">
-                <input type="text" name="pseudo" />
-            </div>
+        <label class="col-sm-4">Pseudo:</label>
+        <div class="col-sm-8">
+            <input type="text" name="pseudo" class="form-control"/>
         </div>
-        <div class="row">
-            <label class="col-sm-3" >Password:</label>
-            <div class = "col-sm-9">
-                <input type="password" name="password" />
-            </div>
-            <div class="row">
-                <label>
-                    <input type="submit" name="submit" value="Valider">
-                </label>
-            </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-4">Password:</label>
+        <div class="col-sm-8">
+            <input type="password" name="password" class="form-control"/>
         </div>
+    </div>
+    <div class="form-group">
+        <input type="submit" name="submit" value="Valider" class="col-sm-12">
     </div>
 </form>
