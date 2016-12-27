c77541cc11222415bf2007f973790d8fb2aae8af
Loïc TORO <loic.toro@alumni.univ-avignon.fr>
2016-12-27 08:44:20 +0100

    Ajout login, manque notification fonctionnelle et form

--- kiwibook2/kiwibook/view/layout/layout.php
+++ kiwibook2/kiwibook/view/layout/layout.php
@@ -2,12 +2,11 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
-	<title>Ton appli !</title>
+	<title>KiwiBook</title>
    
 </head>
 <body>
-<!-- j'ai le droit de mettre des commentaires dans mon fichier HTML -->
-	<h2>Super c'est ton appli ! </h2>
+
 	<?php include($template_view); ?>
     
 </body>
