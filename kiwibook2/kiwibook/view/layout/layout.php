e284f8dedfb606834f081aec808a6267aaefcae0
Loïc TORO <loic.toro@alumni.univ-avignon.fr>
2016-12-28 08:37:35 +0100

    favicon parce que c'est cool

--- kiwibook2/kiwibook/view/layout/layout.php
+++ kiwibook2/kiwibook/view/layout/layout.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>KiwiBook</title>
-   
+
+    <link rel="icon" type="image/x-icon" href="images/favicon.png"/>
 </head>
 <body>
 	<?php include('bandeauNotif.php'); ?>
